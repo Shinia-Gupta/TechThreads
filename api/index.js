7bef573bc3553e04a72599377bff4306bbef0a67
Shinia Gupta <shiniavit@gmail.com>
2024-05-25 22:05:04 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import { db } from './dbConnect.js';
 import userRoutes from './routes/user.routes.js';
+import authRoutes from './routes/auth.routes.js';
 db();
 const app=express();
+app.use(express.json());
 
 app.listen(3000,()=>{
     console.log("Server is listening at port 3000!!");
 })
 
 app.use('/api/user',userRoutes)
+app.use('/api/auth',authRoutes)
