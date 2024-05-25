51b4c15bfdfada95e34349cead2e61eb3f1792bb
Shinia Gupta <shiniavit@gmail.com>
2024-05-25 19:55:38 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { db } from './dbConnect.js';
-
+import userRoutes from './routes/user.routes.js';
 db();
 const app=express();
 
 app.listen(3000,()=>{
     console.log("Server is listening at port 3000!!");
 })
+
+app.use('/api/user',userRoutes)
