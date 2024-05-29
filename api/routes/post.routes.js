faa74cf6de538d94c2d5b971d178e5154940dcbc
Shinia Gupta <shiniavit@gmail.com>
2024-05-29 19:41:49 +0530

    create getPosts api route

--- api/routes/post.routes.js
+++ api/routes/post.routes.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import { verifyToken } from '../middlewares/verifyUser.middleware.js';
-import { createPost } from '../controllers/post.controller.js';
+import { createPost, getPosts } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
 
 router.post('/create-post',verifyToken,createPost);
-// router.post('/signin',signin);
+router.get('/getPosts',getPosts);
 // router.post('/google',signinWithGoogle);
 
 export default router;
